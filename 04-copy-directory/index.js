17f9e4080e791cbf8290dc0ae6db9a27a23c1cf9
Snischuk Maxim <m.snischuk@gmail.com>
2024-01-22 13:19:55 +0200

    fix: improve copying nested directories

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fsPromise = require('fs/promises');
 const path = require('path');
 
 const FOLDER_TO_COPY = 'files';
@@ -7,58 +7,44 @@ const FOLDER_COPIED = 'files-copy';
 const pathFolderToCopy = path.join(__dirname, FOLDER_TO_COPY);
 const pathFolderCopied = path.join(__dirname, FOLDER_COPIED);
 
-function clearDir(directory) {
-  fs.readdir(directory, (err, files) => {
-    if (err) return console.error(err);
+async function copyDir(source, destination) {
+  try {
+    await removeDir(destination);
+    await fsPromise.mkdir(destination, { recursive: true });
+    const files = await fsPromise.readdir(source);
 
-    files.forEach((file) => {
-      const filePath = path.join(directory, file);
-      deleteFile(filePath);
+    files.forEach(async (file) => {
+      const sourcePath = path.join(source, file);
+      const destinationPath = path.join(destination, file);
+
+      try {
+        const stats = await fsPromise.stat(sourcePath);
+
+        if (stats.isFile()) {
+          await fsPromise.copyFile(sourcePath, destinationPath);
+        }
+
+        if (stats.isDirectory()) {
+          await copyDir(sourcePath, destinationPath);
+        }
+      } catch (error) {
+        return error;
+      }
     });
-  });
+  } catch (error) {
+    return error;
+  }
 }
 
-function deleteFile(filePath) {
-  fs.unlink(filePath, (err) => {
-    if (err) return console.error(err);
-  });
-}
-
-function copyFile(sourcePath, destinationPath) {
-  const readStream = fs.createReadStream(sourcePath);
-  const writeStream = fs.createWriteStream(destinationPath);
-
-  readStream.pipe(writeStream);
-
-  readStream.on('error', (error) => {
-    console.error(
-      `Oops! Error copying file ${path.basename(sourcePath)}: ${error.message}`,
-    );
-  });
-}
-
-function handleReadDir(err, files) {
-  if (err) return console.error(err);
-
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const sourcePath = path.join(pathFolderToCopy, file.name);
-      const destinationPath = path.join(pathFolderCopied, file.name);
-
-      copyFile(sourcePath, destinationPath);
+async function removeDir(directory) {
+  try {
+    const stat = await fsPromise.stat(directory);
+    if (stat.isDirectory()) {
+      await fsPromise.rm(directory, { recursive: true });
     }
-  });
+  } catch (error) {
+    return error;
+  }
 }
 
-function handleMkDir(err) {
-  if (err) return console.error(err);
-
-  clearDir(pathFolderCopied);
-  fs.readdir(pathFolderToCopy, { withFileTypes: true }, handleReadDir);
-}
-
-function copyDir() {
-  fs.mkdir(pathFolderCopied, { recursive: true }, handleMkDir);
-}
-
-copyDir();
+copyDir(pathFolderToCopy, pathFolderCopied);
