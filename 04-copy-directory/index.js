71f898844026726595b32c2204f087ab87876913
Snischuk Maxim <m.snischuk@gmail.com>
2024-01-22 16:32:31 +0200

    refactor: remove unnecessary function removeDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,7 +9,6 @@ const pathFolderCopied = path.join(__dirname, FOLDER_COPIED);
 
 async function copyDir(source, destination) {
   try {
-    await removeDir(destination);
     await fsPromise.mkdir(destination, { recursive: true });
     const files = await fsPromise.readdir(source);
 
@@ -28,22 +27,11 @@ async function copyDir(source, destination) {
           await copyDir(sourcePath, destinationPath);
         }
       } catch (error) {
-        return error;
+        console.error(error.message);
       }
     });
   } catch (error) {
-    return error;
-  }
-}
-
-async function removeDir(directory) {
-  try {
-    const stat = await fsPromise.stat(directory);
-    if (stat.isDirectory()) {
-      await fsPromise.rm(directory, { recursive: true });
-    }
-  } catch (error) {
-    return error;
+    console.error(error.message);
   }
 }
 
